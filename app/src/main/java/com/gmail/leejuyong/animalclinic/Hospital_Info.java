ec61b351c38f69d9de5073d84c8711b322628b09
LeeJuYong <juyonglee0208@gmail.com>
2016-10-30 00:05:38 +0900

    폐업 및 전화번호 유무에 따른 분기 처리

---
+++ app/src/main/java/com/gmail/leejuyong/animalclinic/Hospital_Info.java
@@ -0,0 +1,18 @@
+package com.gmail.leejuyong.animalclinic;
+
+/**
+ * Created by leejuyong on 2016. 10. 29..
+ */
+
+public class Hospital_Info
+{
+    String hospital_Name;
+    String getHospital_Address;
+    String getHospital_Phone;
+
+    public Hospital_Info(String hospital_Name, String getHospital_Address, String getHospital_Phone) {
+        this.hospital_Name = hospital_Name;
+        this.getHospital_Address = getHospital_Address;
+        this.getHospital_Phone = getHospital_Phone;
+    }
+}
--- app/src/main/java/com/gmail/leejuyong/animalclinic/MainActivity.java
+++ app/src/main/java/com/gmail/leejuyong/animalclinic/MainActivity.java
@@ -10,15 +10,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Hospital_Info> hostital_list;
     String Url = "http://openapi.sb.go.kr:8088/4d706b6d796a756e32324952545656/json/SbAnimalHospital/1/100";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hostital_list = new ArrayList<>();
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(Url, null, new JsonHttpResponseHandler(){
@@ -34,11 +39,36 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray pharmacies = objects.getJSONArray("row");
                     for(int i=0; i<pharmacies.length(); i++)
                     {
-                        if(pharmacies.getJSONObject(i).getString("TRD_STATE_GBN_CTN").equals("정상")) {
-                            Log.d("Test", pharmacies.getJSONObject(i).getString("WRKP_NM"));
-                            Log.d("Test", pharmacies.getJSONObject(i).getString("SITE_ADDR"));
-                            Log.d("Test", pharmacies.getJSONObject(i).getString("SITE_TEL"));
+                        if(pharmacies.getJSONObject(i).getString("TRD_STATE_GBN_CTN").equals("정상"))
+                        {
+                            if(pharmacies.getJSONObject(i).getString("SITE_TEL").contains("02-"))
+                            {
+                                Hospital_Info info = new Hospital_Info(pharmacies.getJSONObject(i).getString("WRKP_NM"), pharmacies.getJSONObject(i).getString("SITE_ADDR"), pharmacies.getJSONObject(i).getString("SITE_TEL"));
+                                hostital_list.add(info);
+                            }
+                            else
+                            {
+                                if(pharmacies.getJSONObject(i).getString("SITE_TEL").equals(""))
+                                {
+                                    Hospital_Info info = new Hospital_Info(pharmacies.getJSONObject(i).getString("WRKP_NM"), pharmacies.getJSONObject(i).getString("SITE_ADDR"), "없음");
+                                    hostital_list.add(info);
+                                }
+                                else
+                                {
+                                    Hospital_Info info = new Hospital_Info(pharmacies.getJSONObject(i).getString("WRKP_NM"), pharmacies.getJSONObject(i).getString("SITE_ADDR"), "02-" + pharmacies.getJSONObject(i).getString("SITE_TEL"));
+                                    hostital_list.add(info);
+                                }
+                            }
+
                         }
+                    }
+
+                    //  정보를 로그에 출력하는 기능
+                    for (int num=0; num < hostital_list.size(); num++)
+                    {
+                        Log.d("Test", hostital_list.get(num).hospital_Name);
+                        Log.d("Test", hostital_list.get(num).getHospital_Address);
+                        Log.d("Test", hostital_list.get(num).getHospital_Phone);
                     }
                 }
                 catch (JSONException e)
